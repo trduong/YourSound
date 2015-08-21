317ee1c9aacdc90c9136029869f63573b1f9ff56
trduong <DuongNG@Duongs-MacBook-Air.local>
2015-08-21 13:49:42 -0500

    first

---
+++ YourSounds/Gerne.h
@@ -0,0 +1,17 @@
+//
+//  Gerne.h
+//  YourSounds
+//
+//  Created by Duong Nguyen on 8/21/15.
+//  Copyright (c) 2015 Duong Nguyen. All rights reserved.
+//
+
+#import <Realm/Realm.h>
+
+@interface Gerne : RLMObject
+
+@property NSInteger index;
+@property NSString *gerneName;
+@property NSString *gerneID;
+
+@end
---
+++ YourSounds/Gerne.m
@@ -0,0 +1,16 @@
+//
+//  Gerne.m
+//  YourSounds
+//
+//  Created by Duong Nguyen on 8/21/15.
+//  Copyright (c) 2015 Duong Nguyen. All rights reserved.
+//
+
+#import "Gerne.h"
+
+@implementation Gerne
+
++ (NSString *)primaryKey {
+    return @"index";
+}
+@end
