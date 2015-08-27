5c296bd6702e6185311595369e13315f31a66cb2
trduong <DuongNG@duongs-air.austin.ibm.com>
2015-08-27 14:55:24 -0500

    .....

---
+++ YourSounds/DetailsGerneViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DetailsGerneViewCell.h
+//  YourSounds
+//
+//  Created by Duong Nguyen on 8/27/15.
+//  Copyright (c) 2015 Duong Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsGerneViewCell : UITableViewCell
+
+@end
--- YourSounds/GerneViewController.m
+++ YourSounds/GerneViewController.m
@@ -100,7 +100,7 @@
 }
 */
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -108,6 +108,6 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
+
 
 @end
